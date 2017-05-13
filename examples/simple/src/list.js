f03c075f35f68d559e424bbf02f4babb055dcc9c
Vadim Taits <taizvadim@gmail.com>
2017-05-13 21:58:31 +0300

    Added example for loading list with reduxFilterlist component

---
+++ examples/simple/src/list.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+import {reduxFilterlist} from 'redux-filterlist'
+
+const List = ({
+  listState: {
+    additional,
+    items,
+    loading,
+  },
+}) => (
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>brand</th>
+          <th>owner</th>
+          <th>color</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {
+          items.map(({
+            id,
+            brand,
+            owner,
+            color,
+          }) => (
+            <tr key={ id }>
+              <td>{ id }</td>
+              <td>{ brand }</td>
+              <td>{ owner }</td>
+              <td>{ color }</td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
+
+    {
+      additional && (
+        <h4>
+          Total: { additional.count }
+        </h4>
+      )
+    }
+
+    {
+      loading && (
+        <h3>Loading...</h3>
+      )
+    }
+  </div>
+)
+
+export default reduxFilterlist({
+  listId: 'simple',
+  loadItems: () => fetch('/cars')
+    .then((response) => response.json())
+    .then((cars) => ({
+      items: cars,
+      additional: {
+        count: cars.length,
+      },
+    })),
+})(List)
---
+++ examples/simple/src/mock-api.js
@@ -0,0 +1,32 @@
+import fetchMock from 'fetch-mock'
+
+function withDelay(response, time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(response);
+    }, time)
+  })
+}
+
+const cars = [
+  {
+    id: 1,
+    brand: 'Audi',
+    owner: 'Tom',
+    color: 'yellow',
+  },
+  {
+    id: 2,
+    brand: 'Mercedes',
+    owner: 'Henry',
+    color: 'white',
+  },
+  {
+    id: 3,
+    brand: 'BMW',
+    owner: 'Alex',
+    color: 'black',
+  },
+]
+
+fetchMock.get('/cars', withDelay(cars, 2000))
