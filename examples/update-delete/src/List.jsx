0252312969a2b68be3f4a93d405c8e55a99470c8
Vadim Taits <taizvadim@gmail.com>
2017-11-06 02:42:32 +0300

    Added insertItem example: added create form to update-delete example

--- examples/update-delete/src/List.jsx
+++ examples/update-delete/src/List.jsx
@@ -6,6 +6,71 @@ import {reduxFilterlist, filterlistPropTypes} from 'redux-filterlist'
 import Item, { itemPropTypes } from './Item';
 
 class List extends Component {
+  state = {
+    brand: '',
+    owner: '',
+    color: '',
+  }
+
+  setFormValue(fieldName, event) {
+    this.setState({
+      [fieldName]: event.target.value,
+    });
+  }
+
+  setBrand = (event) => {
+    this.setFormValue('brand', event);
+  }
+
+  setOwner = (event) => {
+    this.setFormValue('owner', event);
+  }
+
+  setColor = (event) => {
+    this.setFormValue('color', event);
+  }
+
+  addItem = (event) => {
+    event.preventDefault();
+
+    const {
+      listState: {
+        items,
+        additional: {
+          count,
+        },
+      },
+
+      insertItem,
+    } = this.props;
+
+    const {
+      brand,
+      owner,
+      color,
+    } = this.state;
+
+    const maxId = items.reduce((res, item) => Math.max(res, item.id), 0);
+
+    const newItem = {
+      id: maxId + 1,
+      checked: false,
+      brand,
+      owner,
+      color,
+    };
+
+    this.props.insertItem(0, newItem, {
+      count: count + 1,
+    });
+
+    this.setState({
+      brand: '',
+      owner: '',
+      color: '',
+    });
+  }
+
   toggleChecked = (itemIndex) => {
     const {
       listState: {
@@ -52,8 +117,67 @@ class List extends Component {
       check,
     } = this.props;
 
+    const {
+      brand,
+      owner,
+      color,
+    } = this.state;
+
     return (
       <div>
+        <form onSubmit={this.addItem}>
+          <div>
+            <label htmlFor="id_brand">
+              Brand:
+            </label>
+
+            <input
+              type="text"
+              id="id_brand"
+              name="brand"
+              value={brand}
+              onChange={this.setBrand}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="id_owner">
+              Owner:
+            </label>
+
+            <input
+              type="text"
+              id="id_owner"
+              name="owner"
+              value={owner}
+              onChange={this.setOwner}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="id_color">
+              Color:
+            </label>
+
+            <input
+              type="text"
+              id="id_color"
+              name="color"
+              value={color}
+              onChange={this.setColor}
+            />
+          </div>
+
+          <button
+            type="submit"
+            disabled={loading}
+          >
+            Add item
+          </button>
+        </form>
+
+        <br />
+
         <table>
           <thead>
             <tr>
