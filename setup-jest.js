7d14c296b31cdffb5433e753aebed1330f492b7c
Vadim Taits <taizvadim@gmail.com>
2018-01-02 15:36:40 +0300

    Fixed warning in tests: updated react and react-dom in devDependencies, added raf polyfill

--- setup-jest.js
+++ setup-jest.js
@@ -1,9 +1,7 @@
+import 'raf/polyfill';
+
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
-global.requestAnimationFrame = function(callback) {
-  setTimeout(callback, 0);
-};
 
 configure({
   adapter: new Adapter(),
